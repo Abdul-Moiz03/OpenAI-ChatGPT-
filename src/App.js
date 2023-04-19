4d3c09bd2f6f36a4798e64d8ccf5cf2555fda220
Abdul Moiz <moizrauf03@gmail.com>
2023-04-19 18:45:55 +0500

    auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import NavBar from "./Components/NavBar";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Register from "./Pages/Register";
+import Login from "./Pages/Login";
+import Toaster from "react-hot-toast";
 
 function App() {
-  return <div className="App">moiz</div>;
+  return (
+    <>
+      <NavBar />
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
